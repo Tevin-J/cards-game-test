95abe56fc1c9031794eb5d3735cf58dddb6963b5
Anton <anton.revta@gmail.com>
2020-05-28 19:57:56 +0300

    Updates

---
+++ precache-manifest.15d7ee340b774787d8ffce50d05f4ce4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4c516eb9c471de030dd87bebffb855",
+    "url": "/cards-game-test/index.html"
+  },
+  {
+    "revision": "541caea87554e2c905c4",
+    "url": "/cards-game-test/static/css/main.271e1857.chunk.css"
+  },
+  {
+    "revision": "d69dbed4b67acf828ecf",
+    "url": "/cards-game-test/static/js/2.9ef100b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards-game-test/static/js/2.9ef100b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541caea87554e2c905c4",
+    "url": "/cards-game-test/static/js/main.eba3015a.chunk.js"
+  },
+  {
+    "revision": "f3846dda32e384f2d3b7",
+    "url": "/cards-game-test/static/js/runtime-main.3c56b2ab.js"
+  },
+  {
+    "revision": "7cba9949a748d2e261c1a7fbb907992f",
+    "url": "/cards-game-test/static/media/braid.7cba9949.svg"
+  },
+  {
+    "revision": "2667d78b2103a5f602bfb16d37b20e10",
+    "url": "/cards-game-test/static/media/cactus.2667d78b.svg"
+  },
+  {
+    "revision": "4f5263e923214a4d326ecfc421619c54",
+    "url": "/cards-game-test/static/media/click.4f5263e9.mp3"
+  },
+  {
+    "revision": "d4c8b5e976fee66f8d73633816f16da4",
+    "url": "/cards-game-test/static/media/cupcake.d4c8b5e9.svg"
+  },
+  {
+    "revision": "7e2f21382d160ce83faf47dc101dfd45",
+    "url": "/cards-game-test/static/media/hat.7e2f2138.svg"
+  },
+  {
+    "revision": "75bf2c2d29581aba7e4c8820b3c09cd9",
+    "url": "/cards-game-test/static/media/hotDog.75bf2c2d.svg"
+  },
+  {
+    "revision": "6c67018b128fe3bc554bf60259c467fa",
+    "url": "/cards-game-test/static/media/quentao.6c67018b.svg"
+  },
+  {
+    "revision": "118acedf1cead9c7e1a0af83052836cd",
+    "url": "/cards-game-test/static/media/scarecrow.118acedf.svg"
+  },
+  {
+    "revision": "2a0c42220df97192e7c69e65cc4936fe",
+    "url": "/cards-game-test/static/media/sunflower.2a0c4222.svg"
+  }
+]);
